8c762c5a2026ad3cb8a47804c378cf214491de1c
Kai Klesatschke <github@klesatschke.net>
2022-10-27 01:48:43 +0200

    Changes form the Threema Message SDK 1.1.4

--- src/main/java/com/neilalexander/jnacl/NaCl.java
+++ src/main/java/com/neilalexander/jnacl/NaCl.java
@@ -1,17 +1,17 @@
 //
 //  Copyright (c) 2011, Neil Alexander T.
 //  All rights reserved.
-// 
+//
 //  Redistribution and use in source and binary forms, with
 //  or without modification, are permitted provided that the following
 //  conditions are met:
-// 
+//
 //  - Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
 //  - Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 //  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 //  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,30 +27,39 @@
 
 package com.neilalexander.jnacl;
 
-import com.neilalexander.jnacl.crypto.curve25519xsalsa20poly1305;
-
+import java.security.SecureRandom;
 import java.util.Formatter;
 
+import com.neilalexander.jnacl.crypto.curve25519xsalsa20poly1305;
+import com.neilalexander.jnacl.crypto.xsalsa20;
+import com.neilalexander.jnacl.crypto.xsalsa20poly1305;
+
 public class NaCl {
-  static final int crypto_secretbox_KEYBYTES = 32;
-  static final int crypto_secretbox_NONCEBYTES = 24;
-  static final int crypto_secretbox_ZEROBYTES = 32;
-  static final int crypto_secretbox_BOXZEROBYTES = 16;
-  static final int crypto_secretbox_BEFORENMBYTES = 32;
+  public static final int PUBLICKEYBYTES = 32;
+  public static final int SECRETKEYBYTES = 32;
+  public static final int BEFORENMBYTES = 32;
+  public static final int NONCEBYTES = 24;
+  public static final int ZEROBYTES = 32;
+  public static final int BOXZEROBYTES = 16;
+  public static final int BOXOVERHEAD = ZEROBYTES - BOXZEROBYTES;
+  public static final int SYMMKEYBYTES = 32;
+  public static final int STREAMKEYBYTES = 32;
 
-  private byte[] precomputed = new byte[crypto_secretbox_BEFORENMBYTES];
+  private final byte[] precomputed = new byte[BEFORENMBYTES];
 
-  public NaCl(byte[] privatekey, byte[] publickey) throws Exception {
-    if (privatekey.length < crypto_secretbox_KEYBYTES)
-      throw new Exception("Private key too short");
+  public NaCl(byte[] privatekey, byte[] publickey) {
+    if (privatekey.length != SECRETKEYBYTES) {
+      throw new Error("Invalid private key length");
+    }
 
-    if (publickey.length < crypto_secretbox_KEYBYTES)
-      throw new Exception("Public key too short");
+    if (publickey.length != PUBLICKEYBYTES) {
+      throw new Error("Invalid public key length");
+    }
 
     curve25519xsalsa20poly1305.crypto_box_beforenm(this.precomputed, publickey, privatekey);
   }
 
-  public NaCl(String privatekey, String publickey) throws Exception {
+  public NaCl(String privatekey, String publickey) {
     this(getBinary(privatekey), getBinary(publickey));
   }
 
@@ -59,11 +68,13 @@ public class NaCl {
   }
 
   public byte[] encrypt(byte[] input, int inputlength, byte[] nonce) {
-    byte[] paddedinput = new byte[inputlength + crypto_secretbox_ZEROBYTES];
-    byte[] output = new byte[inputlength + crypto_secretbox_ZEROBYTES];
+    if (nonce.length != NONCEBYTES) {
+      throw new Error("Invalid nonce length");
+    }
 
-    System.arraycopy(input, 0, paddedinput, crypto_secretbox_ZEROBYTES, inputlength);
-    curve25519xsalsa20poly1305.crypto_box_afternm(output, paddedinput, paddedinput.length, nonce, this.precomputed);
+    byte[] output = new byte[inputlength + BOXOVERHEAD];
+    curve25519xsalsa20poly1305.crypto_box_afternm_nopad(
+        output, 0, input, 0, input.length, nonce, this.precomputed);
 
     return output;
   }
@@ -73,11 +84,161 @@ public class NaCl {
   }
 
   public byte[] decrypt(byte[] input, int inputlength, byte[] nonce) {
-    byte[] paddedoutput = new byte[inputlength];
-    byte[] output = new byte[inputlength - crypto_secretbox_ZEROBYTES];
+    if (nonce.length != NONCEBYTES) {
+      throw new Error("Invalid nonce length");
+    }
 
-    curve25519xsalsa20poly1305.crypto_box_open_afternm(paddedoutput, input, inputlength, nonce, this.precomputed);
-    System.arraycopy(paddedoutput, crypto_secretbox_ZEROBYTES, output, 0, paddedoutput.length - crypto_secretbox_ZEROBYTES);
+    if (inputlength < BOXOVERHEAD) {
+      return null;
+    }
+
+    byte[] output = new byte[inputlength - BOXOVERHEAD];
+    if (curve25519xsalsa20poly1305.crypto_box_open_afternm_nopad(
+            output, 0, input, 0, input.length, nonce, this.precomputed)
+        != 0) {
+      return null;
+    }
+
+    return output;
+  }
+
+  public static void genkeypair(byte[] publickey, byte[] privatekey) {
+    genkeypair(publickey, privatekey, null);
+  }
+
+  public static void genkeypair(byte[] publickey, byte[] privatekey, byte[] seed) {
+    SecureRandom random = new SecureRandom();
+
+    random.nextBytes(privatekey);
+
+    if (seed != null) {
+      if (seed.length != SECRETKEYBYTES) {
+        throw new Error("Invalid seed length");
+      }
+
+      for (int i = 0; i < SECRETKEYBYTES; i++) {
+        privatekey[i] ^= seed[i];
+      }
+    }
+
+    curve25519xsalsa20poly1305.crypto_box_getpublickey(publickey, privatekey);
+  }
+
+  public static byte[] derivePublicKey(byte[] privatekey) {
+    if (privatekey.length != SECRETKEYBYTES) {
+      throw new Error("Invalid private key length");
+    }
+
+    byte[] publickey = new byte[PUBLICKEYBYTES];
+    curve25519xsalsa20poly1305.crypto_box_getpublickey(publickey, privatekey);
+    return publickey;
+  }
+
+  public static byte[] symmetricEncryptData(byte[] input, byte[] key, byte[] nonce) {
+    if (key.length != SYMMKEYBYTES) {
+      throw new Error("Invalid symmetric key length");
+    }
+
+    if (nonce.length != NONCEBYTES) {
+      throw new Error("Invalid nonce length");
+    }
+
+    byte[] output = new byte[input.length + BOXOVERHEAD];
+    xsalsa20poly1305.crypto_secretbox_nopad(output, 0, input, 0, input.length, nonce, key);
+
+    return output;
+  }
+
+  /**
+   * In-place version of {@link #symmetricEncryptData(byte[], byte[], byte[])} that stores the
+   * output in the same byte array as the input. The input data must begin at offset {@link
+   * #BOXOVERHEAD} in the array (the first BOXOVERHEAD bytes are ignored and will be overwritten
+   * with the message authentication code during encryption).
+   *
+   * @param io plaintext on input (starting at offset BOXOVERHEAD), ciphertext on return (full
+   *     array)
+   * @param key encryption key
+   * @param nonce encryption nonce
+   */
+  public static void symmetricEncryptDataInplace(byte[] io, byte[] key, byte[] nonce) {
+
+    if (key.length != SYMMKEYBYTES) {
+      throw new Error("Invalid symmetric key length");
+    }
+
+    if (nonce.length != NONCEBYTES) {
+      throw new Error("Invalid nonce length");
+    }
+
+    if (io.length < BOXOVERHEAD) {
+      throw new Error("Invalid I/O length");
+    }
+
+    xsalsa20poly1305.crypto_secretbox_nopad(
+        io, 0, io, BOXOVERHEAD, io.length - BOXOVERHEAD, nonce, key);
+  }
+
+  public static byte[] symmetricDecryptData(byte[] input, byte[] key, byte[] nonce) {
+    if (key.length != SYMMKEYBYTES) {
+      throw new Error("Invalid symmetric key length");
+    }
+
+    if (nonce.length != NONCEBYTES) {
+      throw new Error("Invalid nonce length");
+    }
+
+    byte[] output = new byte[input.length - BOXOVERHEAD];
+    if (xsalsa20poly1305.crypto_secretbox_open_nopad(output, 0, input, 0, input.length, nonce, key)
+        != 0) {
+      return null;
+    }
+
+    return output;
+  }
+
+  /**
+   * In-place version of {@link #symmetricDecryptData(byte[], byte[], byte[])} that stores the
+   * output in the same byte array as the input. Note that the decrypted output is shorter than the
+   * input, so the last {@link #BOXOVERHEAD} bytes should be ignored in the decrypted output.
+   *
+   * @param io ciphertext on input (full array), plaintext on output (last BOXOVERHEAD bytes set to
+   *     zero)
+   * @param key encryption key
+   * @param nonce encryption nonce
+   * @return decryption successful true/false
+   */
+  public static boolean symmetricDecryptDataInplace(byte[] io, byte[] key, byte[] nonce) {
+    if (key.length != SYMMKEYBYTES) {
+      throw new Error("Invalid symmetric key length");
+    }
+
+    if (nonce.length != NONCEBYTES) {
+      throw new Error("Invalid nonce length");
+    }
+
+    if (io.length < BOXOVERHEAD) {
+      throw new Error("Invalid I/O length");
+    }
+
+    if (xsalsa20poly1305.crypto_secretbox_open_nopad(io, 0, io, 0, io.length, nonce, key) != 0) {
+      return false;
+    }
+
+    /* zeroize last bytes */
+    for (int i = io.length - BOXOVERHEAD; i < io.length; i++) {
+      io[i] = 0;
+    }
+
+    return true;
+  }
+
+  public static byte[] streamCryptData(byte[] input, byte[] key, byte[] nonce) {
+    if (key.length != STREAMKEYBYTES) {
+      throw new Error("Invalid symmetric key length");
+    }
+
+    byte[] output = new byte[input.length];
+    xsalsa20.crypto_stream_xor(output, input, input.length, nonce, key);
 
     return output;
   }
@@ -86,23 +247,29 @@ public class NaCl {
     int len = s.length();
     byte[] data = new byte[len / 2];
 
-    for (int i = 0; i < len; i += 2)
-      data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+    for (int i = 0; i < len; i += 2) {
+      data[i / 2] =
+          (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+    }
 
     return data;
   }
 
   public static String asHex(byte[] buf) {
-    Formatter formatter = new Formatter();
-    for (byte b : buf)
-      formatter.format("%02x", b);
-    return formatter.toString();
+    try (Formatter formatter = new Formatter()) {
+      for (byte b : buf) {
+        formatter.format("%02x", b);
+      }
+      return formatter.toString();
+    }
   }
 
   public static String asHex(int[] buf) {
-    Formatter formatter = new Formatter();
-    for (int b : buf)
-      formatter.format("%02x", b);
-    return formatter.toString();
+    try (Formatter formatter = new Formatter()) {
+      for (int b : buf) {
+        formatter.format("%02x", b);
+      }
+      return formatter.toString();
+    }
   }
 }
--- src/main/java/com/neilalexander/jnacl/crypto/poly1305.java
+++ src/main/java/com/neilalexander/jnacl/crypto/poly1305.java
@@ -1,17 +1,17 @@
 //
 //  Copyright (c) 2011, Neil Alexander T.
 //  All rights reserved.
-// 
+//
 //  Redistribution and use in source and binary forms, with
 //  or without modification, are permitted provided that the following
 //  conditions are met:
-// 
+//
 //  - Redistributions of source code must retain the above copyright notice,
 //    this list of conditions and the following disclaimer.
 //  - Redistributions in binary form must reproduce the above copyright notice,
 //    this list of conditions and the following disclaimer in the documentation
 //    and/or other materials provided with the distribution.
-// 
+//
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 //  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 //  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,152 +27,132 @@
 
 package com.neilalexander.jnacl.crypto;
 
-public class poly1305
-{
-	final int CRYPTO_BYTES = 16;
-	final int CRYPTO_KEYBYTES = 32;
-	
-	static final int[] minusp = {5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 252};
+public class poly1305 {
+  final int CRYPTO_BYTES = 16;
+  final int CRYPTO_KEYBYTES = 32;
 
-	public static int crypto_onetimeauth_verify(byte[] h, int hoffset, byte[] inv, int invoffset, long inlen, byte[] k)
-	{
-		byte[] correct = new byte[16];
-		
-		crypto_onetimeauth(correct, 0, inv, invoffset, inlen, k);
-		return verify_16.crypto_verify(h, hoffset, correct);
-	}
+  static final int[] minusp = {5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 252};
 
-	static void add(int[] h, int[] c)
-	{
-		int j;
-		int u = 0;
-		
-		for (j = 0; j < 17; ++j)
-		{
-			u += h[j] + c[j];
-			h[j] = u & 255;
-			u >>>= 8;
-		}
-	}
+  public static int crypto_onetimeauth_verify(
+      byte[] h, int hoffset, byte[] inv, int invoffset, long inlen, byte[] k) {
+    byte[] correct = new byte[16];
 
-	static void squeeze(int[] h)
-	{
-		int u = 0;
-		
-		for (int j = 0; j < 16; ++j)
-		{
-			u += h[j]; 
-			h[j] = u & 255; 
-			u >>>= 8;
-		}
-		
-		u += h[16];
-		h[16] = u & 3;
-		u = 5 * (u >>> 2);
-		
-		for (int j = 0; j < 16; ++j)
-		{
-			u += h[j];
-			h[j] = u & 255;
-			u >>>= 8;
-		}
-		
-		u += h[16];
-		h[16] = u;
-	}
+    crypto_onetimeauth(correct, 0, inv, invoffset, inlen, k);
+    return verify_16.crypto_verify(h, hoffset, correct);
+  }
 
-	static void freeze(int[] h)
-	{
-		int[] horig = new int[17];
-		
-		for (int j = 0; j < 17; ++j)
-			horig[j] = h[j];
-		
-		add(h, minusp);
-		
-		int negative = (int)(-(h[16] >>> 7));
-		
-		for (int j = 0; j < 17; ++j)
-			h[j] ^= negative & (horig[j] ^ h[j]);
-	}
+  static void add(int[] h, int[] c) {
+    int j;
+    int u = 0;
 
-	static void mulmod(int[] h, int[] r)
-	{
-		int[] hr = new int[17];
-		
-		for (int i = 0; i < 17; ++i)
-		{
-			int u = 0;
-			
-			for (int j = 0; j <= i; ++j) 
-				u += h[j] * r[i - j];
-			
-			for (int j = i + 1; j < 17; ++j) 
-				u += 320 * h[j] * r[i + 17 - j];
-			
-			hr[i] = u;
-		}
-		
-		for (int i = 0; i < 17; ++i)
-			h[i] = hr[i];
-		
-		squeeze(h);
-	}
+    for (j = 0; j < 17; ++j) {
+      u += h[j] + c[j];
+      h[j] = u & 255;
+      u >>>= 8;
+    }
+  }
 
-	public static int crypto_onetimeauth(byte[] outv, int outvoffset, byte[] inv, int invoffset, long inlen, byte[] k)
-	{
-		int j;
-		int[] r = new int[17];
-		int[] h = new int[17];
-		int[] c = new int[17];
+  static void squeeze(int[] h) {
+    int u = 0;
 
-		r[0] = k[0] & 0xFF;
-		r[1] = k[1] & 0xFF;
-		r[2] = k[2] & 0xFF;
-		r[3] = k[3] & 15;
-		r[4] = k[4] & 252;
-		r[5] = k[5] & 0xFF;
-		r[6] = k[6] & 0xFF;
-		r[7] = k[7] & 15;
-		r[8] = k[8] & 252;
-		r[9] = k[9] & 0xFF;
-		r[10] = k[10] & 0xFF;
-		r[11] = k[11] & 15;
-		r[12] = k[12] & 252;
-		r[13] = k[13] & 0xFF;
-		r[14] = k[14] & 0xFF;
-		r[15] = k[15] & 15;
-		r[16] = 0;
+    for (int j = 0; j < 16; ++j) {
+      u += h[j];
+      h[j] = u & 255;
+      u >>>= 8;
+    }
 
-		for (j = 0; j < 17; ++j)
-			h[j] = 0;
+    u += h[16];
+    h[16] = u & 3;
+    u = 5 * (u >>> 2);
 
-		while (inlen > 0)
-		{
-			for (j = 0; j < 17; ++j)
-				c[j] = 0;
-			
-			for (j = 0; (j < 16) && (j < inlen); ++j)
-				c[j] = inv[invoffset + j]&0xff;
-			
-			c[j] = 1;
-			invoffset += j;
-			inlen -= j;
-			add(h, c);
-			mulmod(h, r);
-		}
+    for (int j = 0; j < 16; ++j) {
+      u += h[j];
+      h[j] = u & 255;
+      u >>>= 8;
+    }
 
-		freeze(h);
+    u += h[16];
+    h[16] = u;
+  }
 
-		for (j = 0; j < 16; ++j) 
-			c[j] = k[j + 16] & 0xFF;
-		
-		c[16] = 0;
-		add(h, c);
-		
-		for (j = 0; j < 16; ++j) 
-			outv[j + outvoffset] = (byte)h[j];
-		
-		return 0;
-	}
+  static void freeze(int[] h) {
+    int[] horig = new int[17];
+
+    for (int j = 0; j < 17; ++j) horig[j] = h[j];
+
+    add(h, minusp);
+
+    int negative = (-(h[16] >>> 7));
+
+    for (int j = 0; j < 17; ++j) h[j] ^= negative & (horig[j] ^ h[j]);
+  }
+
+  static void mulmod(int[] h, int[] r) {
+    int[] hr = new int[17];
+
+    for (int i = 0; i < 17; ++i) {
+      int u = 0;
+
+      for (int j = 0; j <= i; ++j) u += h[j] * r[i - j];
+
+      for (int j = i + 1; j < 17; ++j) u += 320 * h[j] * r[i + 17 - j];
+
+      hr[i] = u;
+    }
+
+    for (int i = 0; i < 17; ++i) h[i] = hr[i];
+
+    squeeze(h);
+  }
+
+  public static int crypto_onetimeauth(
+      byte[] outv, int outvoffset, byte[] inv, int invoffset, long inlen, byte[] k) {
+    int j;
+    int[] r = new int[17];
+    int[] h = new int[17];
+    int[] c = new int[17];
+
+    r[0] = k[0] & 0xFF;
+    r[1] = k[1] & 0xFF;
+    r[2] = k[2] & 0xFF;
+    r[3] = k[3] & 15;
+    r[4] = k[4] & 252;
+    r[5] = k[5] & 0xFF;
+    r[6] = k[6] & 0xFF;
+    r[7] = k[7] & 15;
+    r[8] = k[8] & 252;
+    r[9] = k[9] & 0xFF;
+    r[10] = k[10] & 0xFF;
+    r[11] = k[11] & 15;
+    r[12] = k[12] & 252;
+    r[13] = k[13] & 0xFF;
+    r[14] = k[14] & 0xFF;
+    r[15] = k[15] & 15;
+    r[16] = 0;
+
+    for (j = 0; j < 17; ++j) h[j] = 0;
+
+    while (inlen > 0) {
+      for (j = 0; j < 17; ++j) c[j] = 0;
+
+      for (j = 0; (j < 16) && (j < inlen); ++j) c[j] = inv[invoffset + j] & 0xff;
+
+      c[j] = 1;
+      invoffset += j;
+      inlen -= j;
+      add(h, c);
+      mulmod(h, r);
+    }
+
+    freeze(h);
+
+    for (j = 0; j < 16; ++j) c[j] = k[j + 16] & 0xFF;
+
+    c[16] = 0;
+    add(h, c);
+
+    for (j = 0; j < 16; ++j) outv[j + outvoffset] = (byte) h[j];
+
+    return 0;
+  }
 }
